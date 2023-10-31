a6178c332b88f77e61bc013560dcd41adfd9db8f
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-10-31 15:44:53 +0100

    remove cover letter

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 import cv from "./../../pdf/CV_Turelyk_Arsen_10_2023.pdf"
-import coverLetter from "./../../pdf/Cover Letter_Arsen Turelyk.pdf"
+{/*import coverLetter from "./../../pdf/Cover Letter_Arsen Turelyk.pdf"*/}
 
 export default function Header() {
   return (
@@ -12,7 +12,7 @@ export default function Header() {
             <strong>
               Hi, I am <em>Arsen</em>
             </strong>
-            <br />Junior Full-Stack Developer
+            <br />Junior Full Stack Developer
           </h1>
           <div className="header__text">
             <p>with passion for learning and creating.</p>
@@ -20,9 +20,7 @@ export default function Header() {
           <a href={cv} className="btn btn-left">
             Download CV
           </a>
-          <a href={coverLetter} className="btn">
-          Cover letter
-          </a>
+          {/*<a href={coverLetter} className="btn">Cover letter </a>*/}
         </div>
       </header>
     </div>
